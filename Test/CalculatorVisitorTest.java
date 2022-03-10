1313b55459658148d91cc36b28165c274e2b628d
Fogh1206 <esbenvensel.fogh@gmail.com>
2022-03-10 12:59:56 +0100

    Failure of mankind is Unit tests

--- Test/CalculatorVisitorTest.java
+++ Test/CalculatorVisitorTest.java
@@ -1,5 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorVisitorTest {
+    LinkedStack<Token> tokenStack;
+    Operand operand;
+
+    @BeforeEach
+    void setUp() {
+        tokenStack = new LinkedStack<>();
+        Operand operand = new Operand(11);
+    }
+
+    @Test
+    private void pushOperand() {
+        assertTrue(tokenStack.isEmpty());
+        tokenStack.push(operand);
+        assertFalse(tokenStack.isEmpty());
+    }
 
 }
